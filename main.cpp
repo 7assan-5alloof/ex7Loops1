3f71cc1f6c8c019a3bd47e00f62e836cad2efaeb
7assan-5alloof <hassankhalloof20@gmail.com>
2021-03-30 14:57:36 +0400

    Write program

--- main.cpp
+++ main.cpp
@@ -4,6 +4,27 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Press Ctrl-C to stop..." << endl;
+
+    while (true)
+    {
+        // Input 6 numbers to compare with values of variables greatest and smallest
+        int greatest, smallest, input;
+        cout << "Enter a number: ";
+        cin >> input;
+        greatest = input;
+        smallest = input;
+        for (int i = 1; i <= 5; i++)
+        {
+            // Will ask for more input until 6 numbers would have been compared
+            cout << "Enter another number: ";
+            cin >> input;
+            // Will guarantee that any value given for input isn't smaller than the value of smallest or likewise, greater than the value of greatest
+            if (input > greatest) greatest = input;
+            else if (input < smallest) smallest = input;
+        }
+        cout << "Greatest: " << greatest << endl << "Smallest: " << smallest << endl;
+    }
+
     return 0;
 }
